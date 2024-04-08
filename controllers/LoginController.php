0ebf47f0d06ab338a6807a28f9b69b11e5e43cc6
ejprietocal <prietoeider1@gmail.com>
2024-04-08 17:58:03 -0500

    fixin error

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -29,7 +29,7 @@ class LoginController{
                         session_start();
 
                         $_SESSION['id'] = $usuario->id;
-                        $_SESSION['nombre'] = $usuario->nombre . " ". $nombre->apellido;
+                        $_SESSION['nombre'] = $usuario->nombre . " ". $usuario->apellido;
                         $_SESSION['email'] = $usuario->email;
                         $_SESSION['login'] = true;
 
